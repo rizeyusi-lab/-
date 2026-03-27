00dcb47b2bc70525a523a8b6ea81a564e39d7603
na <rizeyusi@gmail.com>
2026-03-27 18:25:06 +0900

    Add files via upload

---
+++ baekjoon/baekjoon/b2743.py
@@ -0,0 +1,15 @@
+inp = input()
+#print(inp)
+print(len(inp))
+
+#inp =input(
+inp ="pulljima"
+#print(inp)
+print(len(inp))
+
+A = int(input())
+B = int(input())
+print(A * (B % 10))        
+print(A * ((B // 10) % 10))  
+print(A * (B // 100))
+print(A * B)    
---
+++ baekjoon/baekjoon/b9498.py
@@ -0,0 +1,13 @@
+s = int(input())
+
+if 90<=s<=100:
+    print('A')
+
+elif 80<=s<=89:
+    print('B')
+
+elif 60<=s<=69:
+    print('C')
+
+else:
+    print('D')
