c506829bb3201742d158c47f66f25b7671295e92
na <rizeyusi@gmail.com>
2026-03-31 18:14:18 +0900

    Add files via upload

---
+++ baekjoon/b3040.py
@@ -0,0 +1,31 @@
+lst = []
+for x in range(9):
+    lst.append(int(input()))
+
+# for i in lst:
+#     print(i, end=' ')
+
+# print('\nsum : ', sum(lst))
+
+nsum = sum(lst)
+reali = 0
+realj = 0
+
+for i in range(9):
+    for j in range(9):
+        if i==j:
+            continue
+        nsum -= (int(lst[i]) + int(lst[j]))
+        # print(lst[i], lst[j], nsum)
+        if nsum == 100:
+            reali = i
+            realj = j
+            break
+        nsum += (int(lst[i]) + int(lst[j]))
+
+# print(reali, realj)
+
+for i in range(len(lst)):
+    if i==reali or i==realj:
+        continue
+    print(lst[i])
